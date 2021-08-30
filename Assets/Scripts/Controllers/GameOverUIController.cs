097a5b522bb8d9d96924a8093b0560459d04e7ce
Pedro de Oliveira <pedrooliveira23@hotmail.com.br>
2021-08-29 22:40:04 -0300

    Correção de bug na tela de Game Over

--- Assets/Scripts/Controllers/GameOverUIController.cs
+++ Assets/Scripts/Controllers/GameOverUIController.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
@@ -11,9 +12,18 @@ public class GameOverUIController : MonoBehaviour
     private GameMasterController gameMasterController;
     void Start()
     {
+        int levelIndex;
+        try
+        {
+            levelIndex = SaveGameDAO.GetInstance().Read().LevelIndex;
+        }
+        catch (FileNotFoundException)
+        {
+            levelIndex = 1;
+        }
         sceneManagerController = GetComponent<SceneManagerController>();
         gameMasterController = GameObject.Find("/GameMaster").GetComponent<GameMasterController>();
-        GameObject.Find("/CanvasMenu/BtnRestart").GetComponent<Button>().onClick.AddListener(sceneManagerController.goToSceneGamePlay);
+        GameObject.Find("/CanvasMenu/BtnRestart").GetComponent<Button>().onClick.AddListener(delegate { sceneManagerController.goToContinueGamePlay(levelIndex); });
         GameObject.Find("/CanvasMenu/BtnQuit").GetComponent<Button>().onClick.AddListener(sceneManagerController.quitGame);
 
         GameObject.Find("/CanvasMenu/TxtLevel").GetComponent<Text>().text = "Nível Alcançado: " + (gameMasterController.getLastLevel() == 0 ? "Máximo" : gameMasterController.getLastLevel().ToString());
